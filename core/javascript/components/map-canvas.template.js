8d5462f26e49591b7a5c12b02560051e3ebff624
Jean Tinland <jean.tinland@gmail.com>
2026-03-28 13:10:51 +0100

    Remove cover elements from map canvas template to simplify structure

--- core/javascript/components/map-canvas.template.js
+++ core/javascript/components/map-canvas.template.js
@@ -3,10 +3,6 @@ export default function template() {
     <div class="canvas">
       <div class="map"></div>
     </div>
-    <div class="cover">
-      <div class="left-cover"></div>
-      <div class="right-cover"></div>
-    </div>
     <map-nomenclature></map-nomenclature>
     <map-scale></map-scale>
     <map-controls></map-controls>
